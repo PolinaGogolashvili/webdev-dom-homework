2646782cceb7008ccf7a0b174b558702a0424467
твое имя < email>
2023-06-17 22:29:46 +0300

    Added a render function to the render.js - file

--- main.js
+++ main.js
@@ -101,37 +101,7 @@ const commentTextClick = () => {
 
 commentTextClick();
 
-const renderComments = () => {
-  const commentsHtml = comments
-    .map((comment, index) => {
-      return `<li data-index="${index}" class="comment">
-<div class="comment-header">
-  <div class="comment-name">${comment.name}</div>
-  <div>${comment.date}</div>
-</div>
-<div class="comment-body">
-  <div class="comment-text">
-   ${comment.text}
-  </div>
-</div>
-<div class="comment-footer">
-  <div class="likes">
-    <span class="likes-counter">${comment.likes}</span>
-    <button data-index="${index}" class="like-button ${
-        comment.isLiked ? "-active-like" : ""
-      }"></button>
-  </div>
-</div>
-</li>`;
-    })
-    .join("");
 
-  listElement.innerHTML = commentsHtml;
-  initEventLike();
-  commentTextClick();
-};
-getComments();
-renderComments();
 
 buttonElement.addEventListener("click", () => {
   buttonElement.disabled = true;
